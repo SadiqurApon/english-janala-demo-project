694bb69e1a0cfc111d7113c867438c5b1cd868ff
SadiqurApon <sadiqurrahmanapon34@gmail.com>
2026-02-24 12:09:55 +0600

    learn vocabolary sention complete

--- script/index.js
+++ script/index.js
@@ -4,6 +4,15 @@ const loadlessons = () => {
         .then(json => displayLessons(json.data))
 };
 const displayLessons = (lessons) => {
-    console.log(lessons);
+    const lessonContainer = document.getElementById("lesson-container");
+    lessonContainer.innerHTML = "";
+    for (let lesson of lessons) {
+        console.log(lesson);
+        const btnDiv = document.createElement("div");
+        btnDiv.innerHTML = `
+        <button class="btn btn-outline btn-primary"><iclass="fa-solid fa-circle-question"></i>Lesson - ${lesson.level_no}</button>
+        `;
+        lessonContainer.appendChild(btnDiv);
+    }
 }
 loadlessons();
